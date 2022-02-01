8e24136e314596804f8633e7573c26ca3411cfc3
darcy <darcy4900@gmail.com>
2022-02-01 10:59:42 -0500

    Initial Commit

---
+++ the motherfucking uh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  the motherfucking uh
+//
+//  Created by Darcy Rollason on 2/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ the motherfucking uh/the_motherfucking_uhApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_motherfucking_uhApp.swift
+//  the motherfucking uh
+//
+//  Created by Darcy Rollason on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct the_motherfucking_uhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
